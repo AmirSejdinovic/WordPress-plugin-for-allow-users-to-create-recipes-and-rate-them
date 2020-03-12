bae0d09dcb7fbbb6542f3905d6ff9eb3f2b57c58
Amir Sejdinovic <amir24V@yahoo.com>
2020-03-12 11:01:38 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+* Plugin Name: Recipe
+* Description: This is a simple WP plugin that allows users to create recipes and reate this recipes
+* Version: 1.0
+* Author: Amir Sejdinovic
+. text domain: recipe
+*/
